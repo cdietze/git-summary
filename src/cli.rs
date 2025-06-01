2730a20575e3aaed0c23fb213ec8adcb2bfe2d17
Christoph Dietze <christoph.dietze@kurzdigital.com>
2025-06-01 22:40:25 +0200

    Simplify a bit

--- src/cli.rs
+++ src/cli.rs
@@ -3,20 +3,20 @@ use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-pub(crate) struct Cli {
+pub struct Cli {
     /// Base directory to search for git repositories
     #[arg(default_value = ".")]
-    pub(crate) dir: PathBuf,
+    pub dir: PathBuf,
 
     /// Time range for commits (e.g. "1 week ago")
     #[arg(short, long, default_value = "1 week ago")]
-    pub(crate) since: String,
+    pub since: String,
 
     /// Author name for filtering commits (defaults to git config user.name)
     #[arg(short, long)]
-    pub(crate) author: Option<String>,
+    pub author: Option<String>,
 
     /// Maximum depth for repository search
     #[arg(short, long, default_value_t = 8)]
-    pub(crate) max_depth: usize,
+    pub max_depth: usize,
 }
